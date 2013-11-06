faa162c6e8b58e1921940fb86b1e487e94e02b9f
rayni84 <rayni84@ville-noumea.nc>
2013-11-06 22:38:26 +0000

    initial commit

---
+++ src/main/java/nc/noumea/mairie/ads/domain/Revision.java
@@ -0,0 +1,10 @@
+package nc.noumea.mairie.ads.domain;
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
+import org.springframework.roo.addon.tostring.RooToString;
+
+@RooJavaBean
+@RooToString
+@RooJpaActiveRecord(identifierColumn = "id_revision", identifierField = "idRevision")
+public class Revision {
+}
