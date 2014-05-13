1aa8e033881136d7eab7c4dfaec83a2f9a180062
rayni84 <rayni84@ville-noumea.nc>
2014-05-13 23:44:06 +0000

    #7695

--- src/main/java/nc/noumea/mairie/ads/repository/TreeRepository.java
+++ src/main/java/nc/noumea/mairie/ads/repository/TreeRepository.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 
 import nc.noumea.mairie.ads.domain.Noeud;
 
@@ -41,4 +42,38 @@ public class TreeRepository implements ITreeRepository {
 	        (BigInteger)(adsEntityManager.createNativeQuery("select nextval('ads_s_noeud_service');").getSingleResult());
 	    return seq.intValue();
 	}
+
+	@Override
+	public Noeud getNoeudFromIdService(int idService, long idRevision) {
+
+		TypedQuery<Noeud> q = adsEntityManager.createNamedQuery("getNoeudFromIdServiceAndRevision", Noeud.class);
+		q.setParameter("idRevision", idRevision);
+		q.setParameter("idService", idService);
+		q.setMaxResults(1);
+
+		List<Noeud> result = q.getResultList();
+
+		if (result.size() == 1)
+			return result.get(0);
+		else
+			return null;
+	}
+
+	@Override
+	public Noeud getNoeudFromCodeServi(String codeServi, long idRevision) {
+
+		TypedQuery<Noeud> q = adsEntityManager.createNamedQuery("getNoeudFromCodeServiAndRevision", Noeud.class);
+		q.setParameter("idRevision", idRevision);
+		q.setParameter("codeServi", codeServi);
+		q.setMaxResults(1);
+
+		List<Noeud> result = q.getResultList();
+
+		if (result.size() == 1)
+			return result.get(0);
+		else
+			return null;
+	}
+
+
 }
