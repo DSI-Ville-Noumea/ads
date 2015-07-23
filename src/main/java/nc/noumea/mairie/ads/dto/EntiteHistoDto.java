d6e8faed9fa208939104c8c38577a13d344c34d7
rebjo84 <rebjo84@ville-noumea.nc>
2015-07-23 02:10:32 +0000

    #16506 ADS : WS qui retourne l'historique d'un noeud

---
+++ src/main/java/nc/noumea/mairie/ads/dto/EntiteHistoDto.java
@@ -0,0 +1,60 @@
+package nc.noumea.mairie.ads.dto;
+
+import java.util.Date;
+
+import org.codehaus.jackson.map.annotate.JsonDeserialize;
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+
+import nc.noumea.mairie.ads.domain.EntiteHisto;
+
+public class EntiteHistoDto extends EntiteDto {
+
+	private Integer idEntiteHisto;
+	@JsonSerialize(using = JsonDateSerializer.class)
+	@JsonDeserialize(using = JsonDateDeserializer.class)
+	private Date dateHisto;
+	private Integer idAgentHisto;
+	private Integer typeHisto;
+	
+	public EntiteHistoDto(EntiteHisto histo) {
+		super.mapEntite(histo);
+		this.idEntiteHisto = histo.getIdEntiteHisto();
+		this.dateHisto = histo.getDateHisto();
+		this.idAgentHisto = histo.getIdAgentHisto();
+		this.typeHisto = null == histo.getType() ? null : histo.getType().getIdRefTypeHisto();
+	}
+
+	public Integer getIdEntiteHisto() {
+		return idEntiteHisto;
+	}
+
+	public void setIdEntiteHisto(Integer idEntiteHisto) {
+		this.idEntiteHisto = idEntiteHisto;
+	}
+
+	public Date getDateHisto() {
+		return dateHisto;
+	}
+
+	public void setDateHisto(Date dateHisto) {
+		this.dateHisto = dateHisto;
+	}
+
+	public Integer getIdAgentHisto() {
+		return idAgentHisto;
+	}
+
+	public void setIdAgentHisto(Integer idAgentHisto) {
+		this.idAgentHisto = idAgentHisto;
+	}
+
+	public Integer getTypeHisto() {
+		return typeHisto;
+	}
+
+	public void setTypeHisto(Integer typeHisto) {
+		this.typeHisto = typeHisto;
+	}
+	
+	
+}
---
+++ src/test/java/nc/noumea/mairie/ads/dto/EntiteHistoDtoTest.java
@@ -0,0 +1,67 @@
+package nc.noumea.mairie.ads.dto;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Date;
+
+import nc.noumea.mairie.ads.domain.EntiteHisto;
+import nc.noumea.mairie.ads.domain.StatutEntiteEnum;
+import nc.noumea.mairie.ads.domain.TypeHistoEnum;
+
+import org.junit.Test;
+
+public class EntiteHistoDtoTest {
+
+	@Test
+	public void EntiteHistoDto_ctor() {
+		
+		// Given
+		EntiteHisto n = new EntiteHisto();
+		n.setIdEntiteHisto(1);
+		n.setDateHisto(new Date());
+		n.setIdAgentHisto(9008487);
+		n.setType(TypeHistoEnum.CREATION);
+		
+		
+		n.setIdEntite(12);
+		n.setSigle("SED-DMD");
+		n.setLabel("SED-DDDDMMMDDDD");
+		n.setTypeEntite("typeEntite");
+
+		n.setLabelCourt("SED");
+		n.setDateCreation(new Date());
+		n.setIdAgentCreation(9005138);
+		n.setDateDeliberationActif(new Date());
+		n.setRefDeliberationActif("refDeliberationActif");
+		n.setDateDeliberationInactif(new Date());
+		n.setRefDeliberationInactif("refDeliberationInactif");
+		n.setDateModification(new Date());
+		n.setIdAgentModification(9002990);
+		n.setStatut(StatutEntiteEnum.ACTIF);
+		
+		// When
+		EntiteHistoDto result = new EntiteHistoDto(n);
+		
+		// Then
+		assertEquals(1, result.getIdEntiteHisto().intValue());
+		assertEquals(n.getDateHisto(), result.getDateHisto());
+		assertEquals(9008487, result.getIdAgentHisto().intValue());
+		assertEquals(TypeHistoEnum.CREATION.getIdRefTypeHisto(), result.getTypeHisto().intValue());
+		
+		assertEquals(12, result.getIdEntite().intValue());
+		assertEquals("SED-DMD", result.getSigle());
+		assertEquals("SED-DDDDMMMDDDD", result.getLabel());
+		assertEquals(0, result.getEnfants().size());
+		
+		assertEquals("SED", result.getLabelCourt());
+		assertEquals(n.getDateDeliberationActif(), result.getDateDeliberationActif());
+		assertEquals("refDeliberationActif", result.getRefDeliberationActif());
+		assertEquals(n.getDateDeliberationInactif(), result.getDateDeliberationInactif());
+		assertEquals("refDeliberationInactif", result.getRefDeliberationInactif());
+		assertEquals(9005138, result.getIdAgentCreation().intValue());
+		assertEquals(n.getDateCreation(), result.getDateCreation());
+		assertEquals(9002990, result.getIdAgentModification().intValue());
+		assertEquals(n.getDateModification(), result.getDateModification());
+		assertEquals(StatutEntiteEnum.ACTIF.getIdRefStatutEntite(), result.getIdStatut().intValue());
+	}
+}
