0a1aa9c0e6626ad678613ccc962c10e38facfc3d
nicno85 <nicno85@ville-noumea.nc>
2015-08-23 23:41:40 +0000

    #17765 : ADS : blinder le code avec la gestion des droits

--- src/test/java/nc/noumea/mairie/ads/service/impl/AccessRightsServiceTest.java
+++ src/test/java/nc/noumea/mairie/ads/service/impl/AccessRightsServiceTest.java
@@ -60,4 +60,52 @@ public class AccessRightsServiceTest {
 		assertTrue(result.getErrors().isEmpty());
 		assertTrue(result.getInfos().isEmpty());
 	}
+
+	@Test
+	public void verifAccessRightEcriture_ReturnFalse() {
+
+		// Given
+		Integer idAgent = 906543;
+		AccessRightOrganigrammeDto droits = new AccessRightOrganigrammeDto();
+		droits.setAdministrateur(false);
+		droits.setEdition(false);
+
+		ISirhWSConsumer sirhWSConsumer = Mockito.mock(ISirhWSConsumer.class);
+		Mockito.when(sirhWSConsumer.getAutorisationOrganigramme(idAgent)).thenReturn(droits);
+
+		AccessRightsService service = new AccessRightsService();
+		ReflectionTestUtils.setField(service, "sirhWsConsumer", sirhWSConsumer);
+
+		// When
+		ReturnMessageDto result = service.verifAccessRightEcriture(idAgent);
+
+		// Then
+		assertFalse(result.getErrors().isEmpty());
+		assertTrue(result.getInfos().isEmpty());
+		assertEquals(result.getErrors().get(0),
+				"Votre identifiant n'a pas les droits nécessaires pour effectuer cette opération.");
+	}
+
+	@Test
+	public void verifAccessRightEcriture_ReturnTrue() {
+
+		// Given
+		Integer idAgent = 906543;
+		AccessRightOrganigrammeDto droits = new AccessRightOrganigrammeDto();
+		droits.setAdministrateur(false);
+		droits.setEdition(true);
+
+		ISirhWSConsumer sirhWSConsumer = Mockito.mock(ISirhWSConsumer.class);
+		Mockito.when(sirhWSConsumer.getAutorisationOrganigramme(idAgent)).thenReturn(droits);
+
+		AccessRightsService service = new AccessRightsService();
+		ReflectionTestUtils.setField(service, "sirhWsConsumer", sirhWSConsumer);
+
+		// When
+		ReturnMessageDto result = service.verifAccessRightEcriture(idAgent);
+
+		// Then
+		assertTrue(result.getErrors().isEmpty());
+		assertTrue(result.getInfos().isEmpty());
+	}
 }
